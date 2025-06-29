536c6eb57b78df0aac788cafff76602e9552666e
goyallucky <luckygoyal221982@gmail.com>
2025-06-29 15:01:27 +0530

    url changed

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-import express from 'express'
 import cors from 'cors'
+import express from 'express'
+import connectToDatabase from './db/db.js'
+import attendanceRouter from './routes/attendance.js'
 import authRouter from "./routes/auth.js"
+import dashboardRouter from "./routes/dashboard.js"
 import departmentRouter from "./routes/department.js"
 import employeeRouter from "./routes/employee.js"
-import connectToDatabase from './db/db.js'
-import salaryRouter from "./routes/salary.js"
 import leaveRouter from "./routes/leave.js"
+import salaryRouter from "./routes/salary.js"
 import settingRouter from "./routes/setting.js"
-import attendanceRouter from './routes/attendance.js'
-import dashboardRouter from "./routes/dashboard.js"
 
 connectToDatabase()
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: "https://worknest-frontend-orpin.vercel.app",
+    credentials: true
+}))
 app.use(express.json())
 app.use(express.static('public/uploads'))
 app.use('/api/auth', authRouter)
